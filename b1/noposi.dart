9c6d2a9da60bb880d4d8395867279deb6436bb44
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-11-27 12:04:18 +0530

    update new quastions

---
+++ b1/noposi.dart
@@ -0,0 +1,31 @@
+// // Write a program to check if a given number is  positive
+
+void main() {
+  double usernNumber = 5;
+  checkPositive(usernNumber);
+}
+
+void checkPositive(double number) {
+  if (number > 0) {
+    print("The number is positive.");
+  } else if (number == 0) {
+    print("The number is zero.");
+  } else {
+    print("The namber is negative.");
+  }
+}
+
+// void main() {
+//   double usernumber = -5;
+//   checknumber(usernumber);
+// }
+
+// void checknumber(double num) {
+//   if (num > 0) {
+//     print("The num is positive");
+//   } else if (num == 0) {
+//     print("The num is zero");
+//   } else {
+//     print("The num is negative");
+//   }
+// }
