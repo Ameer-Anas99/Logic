493303b7140ad502c8f71d5cf58f45569631d3c1
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-11-27 16:32:38 +0530

    update

---
+++ b1/evengreater.dart
@@ -0,0 +1,23 @@
+// 5.	Check if a number is even and greater than 20.
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter a Number:");
+  String userinput = stdin.readLineSync()!;
+  int number = int.parse(userinput);
+
+  if (number % 2 == 0 && number > 20) {
+    print("$number is even and greater than 20.");
+  } else {
+    print("$number does not meet the conditions (even and greater than 20).");
+  }
+}
+
+// void main() {
+//   int number = 22;
+//   if (number % 2 == 0 && number > 20) {
+//     print("$number is even and greater than 20.");
+//   } else {
+//     print("$number does not meet the conditions (even and greater than 20).");
+//   }
+// }
--- b1/geaterthan.dart
+++ b1/geaterthan.dart
@@ -1,22 +1,22 @@
-// import 'dart:io';
-
-// void main() {
-//   stdout.write('Enter a Number: ');
-//   String userinput = stdin.readLineSync()!;
-//   double number = double.parse(userinput);
-
-//   if (number > 10) {
-//     print("$number is greather than 10.");
-//   } else {
-//     print("$number is not greather than 10.");
-//   }
-// }
+import 'dart:io';
 
 void main() {
-  int morethan = 9;
-  if (morethan >= 10) {
-    print('more');
+  stdout.write('Enter a Number: ');
+  String userinput = stdin.readLineSync()!;
+  double number = double.parse(userinput);
+
+  if (number > 10) {
+    print("$number is greather than 10.");
   } else {
-    print('less');
+    print("$number is not greather than 10.");
   }
 }
+
+// void main() {
+//   int morethan = 9;
+//   if (morethan > 10) {
+//     print('more');
+//   } else {
+//     print('less');
+//   }
+// }
