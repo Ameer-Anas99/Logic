ba1fd2a61e4c0a423f2d19cbb4133a47ab831171
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-11-28 14:53:03 +0530

    update

--- b1/infinite_loop.dart
+++ b1/infinite_loop.dart
@@ -1,6 +1,6 @@
 void main() {
   // Infinite loop
   while (true) {
-    print('This is an infinite loop');
+    print('This is an infinite loop. Press Ctrl+C to exit.');
   }
 }
---
+++ b1/sum.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter the value of a:   ");
+  int a = int.parse(stdin.readLineSync()!);
+
+// sum using a loop
+
+  int sum = 0;
+  for (int i = 1; i <= a; i++) {
+    sum += i;
+  }
+
+  // output
+
+  print("Sum of the first $a natural number is : $sum");
+}
