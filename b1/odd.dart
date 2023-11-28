1b53396911427ad296a0ca90ca8b3d3063e9b3fc
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-11-28 16:08:19 +0530

    update

---
+++ b1/odd.dart
@@ -0,0 +1,8 @@
+// Write a program to print odd numbers between 10 and 30 using a loop.
+
+void main() {
+  print("Odd number between 10 and 30:");
+  for (int i = 11; i <= 30; i += 2) {
+    print(i);
+  }
+}
