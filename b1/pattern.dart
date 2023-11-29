797bcdaff8bb40fc48ae1b73a160bd502c1355a7
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-11-29 16:29:33 +0530

    update

--- b1/pattern.dart
+++ b1/pattern.dart
@@ -3,10 +3,33 @@
 //  Printing a Right Triangle:
 // ---------------------------
 
+// import 'dart:io';
+
+// void printRightTriangle(int height) {
+//   for (int i = 1; i <= height; i++) {
+//     for (int j = 1; j <= i; j++) {
+//       stdout.write('*');
+//     }
+//     print('');
+//   }
+// }
+
+// void main() {
+//   print("Enter the height of the right triangle:");
+//   int height = int.parse(stdin.readLineSync()!);
+
+//   printRightTriangle(height);
+// }
+
+// ----------------------------------
+
+// Printing an Upside-Down Right Triangle:
+// --------------------------------------
+
 import 'dart:io';
 
-void printRightTriangle(int height) {
-  for (int i = 1; i <= height; i++) {
+void printUpsideDownRightTriangle(int height) {
+  for (int i = height; i >= 1; i--) {
     for (int j = 1; j <= i; j++) {
       stdout.write('*');
     }
@@ -15,8 +38,8 @@ void printRightTriangle(int height) {
 }
 
 void main() {
-  print("Enter the height of the right triangle:");
+  print("Enter the height of the upside-down right triangle:");
   int height = int.parse(stdin.readLineSync()!);
 
-  printRightTriangle(height);
+  printUpsideDownRightTriangle(height);
 }
