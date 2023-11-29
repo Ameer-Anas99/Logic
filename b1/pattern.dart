7240142058e7e3b9a394ad82b5b3be738b31d995
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-11-29 16:04:34 +0530

    update

---
+++ b1/pattern.dart
@@ -0,0 +1,19 @@
+//  Create a program that uses loops to print various patterns, such as triangles or rectangles
+
+import 'dart:io';
+
+void printRightTriangle(int height) {
+  for (int i = 1; i <= height; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write('*');
+    }
+    print('');
+  }
+}
+
+void main() {
+  print("Enter the height of the right triangle:");
+  int height = int.parse(stdin.readLineSync()!);
+
+  printRightTriangle(height);
+}
