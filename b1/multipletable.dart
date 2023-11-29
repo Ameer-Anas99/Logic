886194bfd9b4c4aefe64fe0c2e61a9b6583a1c6c
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-11-29 16:49:42 +0530

    update

--- b1/multipletable.dart
+++ b1/multipletable.dart
@@ -1,13 +1,19 @@
 // Create a program to generate the multiplication table for a given number using a loop.
-import 'dart:io';
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Enter the number for the multiplication table:");
+//   int number = int.parse(stdin.readLineSync()!);
+
+//   print("\n multiplication for  $number:");
+//   for (int i = 1; i <= 10; i++) {
+//     int result = number * i;
+//     print("$number * $i = $result");
+//   }
+// }
 
 void main() {
-  stdout.write("Enter the number for the multiplication table:");
-  int number = int.parse(stdin.readLineSync()!);
-
-  print("\n multiplication for  $number:");
-  for (int i = 1; i <= 10; i++) {
-    int result = number * i;
-    print("$number * $i = $result");
+  for (int i = 10; i >= 1; i--) {
+    print(i);
   }
 }
