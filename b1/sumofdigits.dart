8c41fb071dbdcc4f10b219ccd613b9a9797d2e61
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-11-29 15:33:14 +0530

    update

--- b1/sumofdigits.dart
+++ b1/sumofdigits.dart
@@ -1,19 +1,5 @@
-// import 'dart:io';
+// Create a program to calculate the sum of the digits of a given number using a loop.
 
-// void main() {
-//   // Input: Read the number from the user
-//   stdout.write("Enter a number: ");
-//   int number = int.parse(stdin.readLineSync()!);
-
-//   int sum = 0;
-//   int tempNumber = number;
-//   while (tempNumber != 0) {
-//     int digit = tempNumber % 10;
-//     sum += digit;
-//     tempNumber ~/= 10;
-//   }
-//   print("sum of digits of $number is: $sum");
-// }
 import 'dart:io';
 
 void main() {
@@ -21,7 +7,6 @@ void main() {
   stdout.write("Enter a number: ");
   int number = int.parse(stdin.readLineSync()!);
 
-  // Calculate the sum of digits using a loop
   int sum = 0;
   int tempNumber = number;
 
@@ -30,7 +15,5 @@ void main() {
     sum += digit;
     tempNumber ~/= 10;
   }
-
-  // Output: Display the sum of digits
-  print("Sum of digits of $number is: $sum");
+  print("sum of digits of $number is: $sum");
 }
