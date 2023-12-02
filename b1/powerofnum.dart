e9b4213d4dc6542e4ea19df04d5cfef9c9122d80
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-02 17:46:18 +0530

    update

--- b1/powerofnum.dart
+++ b1/powerofnum.dart
@@ -1,8 +1,18 @@
 void main() {
-  int base = 5;
-  int exponent = 15;
-  int result = calculatePower(base, exponent);
-  print('$base^$exponent = $result');
+//   int base = 5;
+//   int exponent = 15;
+//   int result = calculatePower(base, exponent);
+//   print('$base^$exponent = $result');
+// }
+
+// int calculatePower(int base, int exponent) {
+//   int result = 1;
+//   for (int i = 0; i < exponent; i++) {
+//     result *= base;
+//   }
+//   return result;
+
+  print(calculatePower(4, 4));
 }
 
 int calculatePower(int base, int exponent) {
