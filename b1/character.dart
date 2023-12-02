fc5c25376c1f50804195f44a0c8afde381a02310
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-02 17:51:35 +0530

    update

---
+++ b1/character.dart
@@ -0,0 +1,10 @@
+void main() {
+  String inputstring = "Ameer Anas";
+  printCharacters(inputstring);
+}
+
+void printCharacters(String input) {
+  for (int i = 0; i < input.length; i++) {
+    print(input[i]);
+  }
+}
