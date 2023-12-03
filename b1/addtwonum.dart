9037820e502a43e175096816be3d33377bccc6f6
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-03 10:55:23 +0530

    update

---
+++ b1/addtwonum.dart
@@ -0,0 +1,10 @@
+void main() {
+  double num1 = 4.9;
+  double num2 = 8.1;
+  double result = addNumbers(num1, num2);
+  print("The sum of $num1 and $num2 is : $result");
+}
+
+double addNumbers(double a, double b) {
+  return a + b;
+}
