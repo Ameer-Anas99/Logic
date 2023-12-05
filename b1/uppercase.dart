f74be7b0afe19aa88240e66cc3d0fbfc1d7b7752
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-05 23:50:50 +0530

    update

---
+++ b1/uppercase.dart
@@ -0,0 +1,11 @@
+//	Write a program to convert a string to Upper Case letters.
+void main() {
+  String originalString = "ameer anas";
+
+  // Convert the string to uppercase
+  String uppercaseString = originalString.toUpperCase();
+
+  // Printing the result
+  print("Original String: $originalString");
+  print("Uppercase String: $uppercaseString");
+}
