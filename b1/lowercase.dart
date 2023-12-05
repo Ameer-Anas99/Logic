1579941cee84c5265d2bfea76bc9350ba9616bf6
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-05 23:53:32 +0530

    update

---
+++ b1/lowercase.dart
@@ -0,0 +1,12 @@
+// Write a program to convert a string to lower case letters.
+
+void main() {
+  String originalString = "AMEER ANAS";
+
+  // Convert the string to lowercase
+  String lowercaseString = originalString.toLowerCase();
+
+  // Printing the result
+  print("Original String: $originalString");
+  print("Lowercase String: $lowercaseString");
+}
