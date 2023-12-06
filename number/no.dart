bb0e14562beabc4f50168bf8be5988bce9115217
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-06 16:31:35 +0530

    update

--- number/no.dart
+++ number/no.dart
@@ -1,13 +1,13 @@
 // print numbers from 1 to 1000.
-// void main() {
-//   for (int i = 1; i <= 1000; i++) {
-//     print(i);
-//   }
-// }
-
-// print n(umbers from 1000 to 1.
 void main() {
-  for (int i = 1000; i >= 1; i--) {
+  for (int i = 1; i <= 1000; i++) {
     print(i);
   }
 }
+
+// print numbers from 1000 to 1.
+// void main() {
+//   for (int i = 1000; i >= 1; i--) {
+//     print(i);
+//   }
+// }
--- number/sum.dart
+++ number/sum.dart
@@ -1,6 +1,6 @@
 void main() {
   int sum = 0;
-  for (int i = 1; i <= 1000; i++) {
+  for (int i = 1; i <= 100; i++) {
     sum += i;
   }
   print("sum $sum");
