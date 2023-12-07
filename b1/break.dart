fddd9f19eb7d155d78a04ae6bf9a043493a36d0e
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-07 14:02:22 +0530

    update

---
+++ b1/break.dart
@@ -0,0 +1,14 @@
+void main() {
+  for (int i = 0; i <= 20; i++) {
+    if (i == 12) continue;
+
+    print(i);
+  }
+}
+
+// void main() {
+//   for (int i = 0; i <= 30; i++) {
+//     if (i == 12) break;
+//     print(i);
+//   }
+// }
