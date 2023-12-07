2a892e78962671575f80506ab1adf51c835048ba
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-07 11:08:09 +0530

    update

--- pattern/1.dart
+++ pattern/1.dart
@@ -4,10 +4,10 @@ void main() {
   int i, j;
   for (i = 1; i <= 5; i++) {
     for (j = 1; j <= 5; j++) {
-      if ((i + j <= 5))
+      if ((i + j <= 5)) {
         stdout.write(' ');
-      else
-        stdout.write(" i");
+      } else
+        stdout.write(' *');
     }
     stdout.writeln(' \n');
   }
--- pattern/alpha.dart
+++ pattern/alpha.dart
@@ -3,8 +3,12 @@ import 'dart:io';
 void main() {
   for (int i = 0; i <= 5; i++) {
     for (int j = 0; j <= i; j++) {
-      stdout.write(String.fromCharCode('A '.codeUnitAt(0) + i));
+      stdout.write(String.fromCharCode("A".codeUnitAt(0) + i));
     }
     stdout.write("\n");
   }
+
+//   String a;
+//   a = "AMEER";
+//   print(String.fromCharCode(65));
 }
