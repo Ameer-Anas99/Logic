ddaf57842048beabee332986ba16fbb3a77904bd
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-08 14:14:02 +0530

    update

--- b1/length.dart
+++ b1/length.dart
@@ -1,8 +1,7 @@
+//	Write a program to find the length of a given string. in dart
 void main() {
-  // Replace this string with the one you want to find the length of
   String myString = "Ameer_Anas";
 
-  // Find the length of the string using the length property
   int lengthOfString = myString.length;
 
   print("Length of the string: $lengthOfString");
---
+++ b1/trim.dart
@@ -0,0 +1,9 @@
+// String Trimming: Write a program to remove leading and trailing spaces from a given string
+void main() {
+  String myString = "   Ameer Anas   ";
+
+  String trimmedString = myString.trim();
+
+  print("Original String: '$myString'");
+  print("Trimmed String: '$trimmedString'");
+}
