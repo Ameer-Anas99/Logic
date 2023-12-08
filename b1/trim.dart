9058d521daad06689cd233226e8fbd4b5a4cee06
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-08 14:25:18 +0530

    update

--- b1/trim.dart
+++ b1/trim.dart
@@ -1,9 +1,16 @@
-// String Trimming: Write a program to remove leading and trailing spaces from a given string
+// // String Trimming: Write a program to remove leading and trailing spaces from a given string
+// void main() {
+//   String myString = "   Ameer Anas   ";
+
+//   String trimmedString = myString.trim();
+
+//   print("Original String: '$myString'");
+//   print("Trimmed String: '$trimmedString'");
+// }
+
 void main() {
-  String myString = "   Ameer Anas   ";
-
-  String trimmedString = myString.trim();
-
-  print("Original String: '$myString'");
-  print("Trimmed String: '$trimmedString'");
+  String a = "    Ameer Anas   ";
+  String b = a.trim();
+  print("$a");
+  print("$b");
 }
