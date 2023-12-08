2b2a8929d1f12366d8f082bf305738b7888a157c
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-08 14:08:28 +0530

    update

---
+++ b1/length.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Replace this string with the one you want to find the length of
+  String myString = "Ameer_Anas";
+
+  // Find the length of the string using the length property
+  int lengthOfString = myString.length;
+
+  print("Length of the string: $lengthOfString");
+}
