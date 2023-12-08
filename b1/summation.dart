0ec9cd58ab896864f012022f1310988a8def8653
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-08 15:21:14 +0530

    update

--- b1/summation.dart
+++ b1/summation.dart
@@ -1,5 +1,8 @@
-int summation(int num) {
-  return List<int>.generate(num, (index) => index + 1).reduce((a, b) => a + b);
+// int summation(int num) {
+//   return List<int>.generate(num, (index) => index + 1).reduce((a, b) => a + b);
+// }
+int summation(int n) {
+  return n * (n + 1) ~/ 2;
 }
 
 void main() {
