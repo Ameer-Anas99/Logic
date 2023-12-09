84fc729edf99b24dd234f3f2c9e0d64bbf2f0a83
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-09 13:23:36 +0530

    update

--- b1/oddeven.dart
+++ b1/oddeven.dart
@@ -9,15 +9,53 @@
 //   }
 // }
 
-import 'dart:io';
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Enter your Number:");
+//   String userinput = stdin.readLineSync()!;
+//   int number = int.parse(userinput);
+//   if (number % 2 == 0) {
+//     print('$number is even.');
+//   } else {
+//     print('$number is odd.');
+//   }
+// }
+
+// void main() {
+//   int num = 8;
+//   if ((num & 1) == 0) {
+//     print('$num is even');
+//   } else {
+//     print('$num is odd');
+//   }
+
+// void main() {
+//   a(4);
+//   a(3);
+//   a(6);
+//   a(7);
+// }
+
+// void a(int num) {
+//   String result = (num % 2 == 0) ? 'even' : 'odd';
+//   print("$num is $result");
+// }
 
 void main() {
-  stdout.write("Enter your Number:");
-  String userinput = stdin.readLineSync()!;
-  int number = int.parse(userinput);
-  if (number % 2 == 0) {
-    print('$number is even.');
-  } else {
-    print('$number is odd.');
+  a(8);
+  a(7);
+  a(9);
+  a(2);
+}
+
+void a(int num) {
+  switch (num % 2) {
+    case 0:
+      print('$num is even ');
+      break;
+    case 1:
+      print('$num is odd');
+      break;
   }
 }
--- b1/palindrome.dart
+++ b1/palindrome.dart
@@ -3,26 +3,20 @@
 import 'dart:io';
 
 bool isPalindrome(int number) {
-  // Convert the number to a string for easy comparison
   String numberStr = number.toString();
 
-  // Use two pointers to iterate from both ends of the string
   int start = 0;
   int end = numberStr.length - 1;
 
-  // Continue checking until the pointers meet in the middle
   while (start < end) {
-    // If the characters at the pointers are not equal, it's not a palindrome
     if (numberStr[start] != numberStr[end]) {
       return false;
     }
 
-    // Move the pointers towards the center
     start++;
     end--;
   }
 
-  // If the loop completes without returning false, it's a palindrome
   return true;
 }
 
