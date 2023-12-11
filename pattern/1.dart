090e8a2371f2ac074481caf0aaf2937591cbe2b2
Ameer-Anas99 <ameeranas19041999@gmail.com>
2023-12-11 12:36:29 +0530

    update

--- pattern/1.dart
+++ pattern/1.dart
@@ -1,14 +1,16 @@
 import 'dart:io';
 
-void main() {
-  int i, j;
-  for (i = 1; i <= 5; i++) {
-    for (j = 1; j <= 5; j++) {
-      if ((i + j <= 5)) {
-        stdout.write(' ');
-      } else
-        stdout.write(' *');
-    }
-    stdout.writeln(' \n');
-  }
-}
+// void main() {
+//   int i, j;
+//   for (i = 1; i <= 5; i++) {
+//     for (j = 1; j <= 5; j++) {
+//       if ((i + j <= 5)) {
+//         stdout.write(' ');
+//       } else
+//         stdout.write(' *');
+//     }
+//     stdout.writeln(' \n');
+//   }
+// }
+
+
