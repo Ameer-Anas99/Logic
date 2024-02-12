4ddda87b13958007fee80c39fe69feccdefe4936
Ameer-Anas99 <ameeranas19041999@gmail.com>
2024-02-12 11:23:33 +0530

    update

---
+++ dart/probability.dart
@@ -0,0 +1,8 @@
+
+// Dartboard Probability:
+void main() {
+  int totalSections = 20;
+  int evenSections = totalSections ~/ 2;
+  double probability = evenSections / totalSections;
+  print('Probability of hitting an even-numbered section: $probability');
+}
