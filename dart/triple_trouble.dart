135f01f5cabcdf54dd14d8c4a2bb314367783748
Ameer-Anas99 <ameeranas19041999@gmail.com>
2024-02-13 02:23:36 +0530

    update

---
+++ dart/triple_trouble.dart
@@ -0,0 +1,8 @@
+// Triple Trouble:
+void main() {
+  int triple20 = 3 * 20;
+  int triple19 = 3 * 19;
+  int triple18 = 3 * 18;
+  int totalScore = triple20 + triple19 + triple18;
+  print('Total score: $totalScore');
+}
