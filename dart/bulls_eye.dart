55c20144f6b77bc903285459c38cca6b5adee01c
Ameer-Anas99 <ameeranas19041999@gmail.com>
2024-02-15 17:57:23 +0530

    update

---
+++ dart/bulls_eye.dart
@@ -0,0 +1,8 @@
+// Bulls-Eye Precision:
+void main() {
+  double ratio = 3;
+  double bullsEyeArea =
+      1; // Assuming the bulls-eye area is 1 unit for simplicity
+  double dartboardArea = ratio * bullsEyeArea;
+  print('Dartboard Area: $dartboardArea, Bulls-Eye Area: $bullsEyeArea');
+}
