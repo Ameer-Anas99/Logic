d5417c9076fcbb589a848df6f2ea3b51130264f6
Ameer-Anas99 <ameeranas19041999@gmail.com>
2024-02-17 11:35:37 +0530

    update

---
+++ dart/number_combination.dart
@@ -0,0 +1,9 @@
+// Number Combination
+
+void main() {
+  int number = 14;
+  int adjacentNumber1 = 11;
+  int adjacentNumber2 = 8;
+  int sum = adjacentNumber1 + adjacentNumber2;
+  print('Sum of numbers on either side of $number: $sum');
+}
